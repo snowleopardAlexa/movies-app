1cc5b2c0787fd3fb87cf8b14c845c17829baf5f8
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 16:02:54 -0500

    add img_300

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -1,9 +1,10 @@
-import './siglecontent.css';
+import './singlecontent.css';
+import { img_300 } from "../../config/config";
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average}) => {
   return (
      <div>
-
+       <img src={`${img_300}/${poster}`} alt="" />
      </div>
   );
 };
