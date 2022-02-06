134d0654f703536892cc932a72de2650c4b96614
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 16:12:33 -0500

    add css to trending

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -3,7 +3,7 @@ import { img_300, unavailable } from "../../config/config";
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average}) => {
   return (
-     <div>
+     <div className="media">
        <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
        <b className="title">{title}</b>
        <span className="subtitle">
