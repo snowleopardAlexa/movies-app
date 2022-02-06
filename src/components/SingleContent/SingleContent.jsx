a75e427b1facf3d0c9567c79e2c8b544b1291a69
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 16:07:14 -0500

    add data and class names in single content

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -4,7 +4,13 @@ import { img_300, unavailable } from "../../config/config";
 const SingleContent = ({ id, poster, title, date, media_type, vote_average}) => {
   return (
      <div>
-       <img src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+       <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+       <b className="title">{title}</b>
+       <span className="subtitle">
+           {media_type === "tv" ? "TV Series" : "Movie"}
+           <span className="subtitle">{date}</span>
+
+       </span>
      </div>
   );
 };
