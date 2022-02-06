bf56c34740e6ab2a48688ce27490a73a9f7205bb
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 10:16:17 -0500

    add array of fake movies and siaply it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Movie from './components/Movie';
 
 function App() {
+
+  const movies = ["1", "2", "3"];
+
   return (
-    <div className="App">
-       
+    <div className="app">
+       {movies.map((movie) => (
+         <Movie />
+       ))}
     </div>
   );
 }
