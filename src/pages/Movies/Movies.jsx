2d355e1e759128585dfc688dd41ba25c5e1ab6bb
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 13:15:43 -0500

    add pages and components: series, search, trending, movies

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,9 @@
+const Movies = () => {
+    return (
+     <div>
+         <span className="pageTitle">M</span>
+     </div>
+    );
+  };
+  
+  export default Movies;
---
+++ src/pages/Series/Series.jsx
@@ -0,0 +1,9 @@
+const Series = () => {
+    return (
+     <div>
+         <span className="pageTitle"><Select:d></Select:d></span>
+     </div>
+    );
+  };
+  
+  export default Series;
