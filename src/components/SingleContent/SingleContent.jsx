d88c2f77f941a0b8b986e58f41d04eba2245292d
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 17:05:02 -0500

    add badge vote

--- src/components/SingleContent/SingleContent.jsx
+++ src/components/SingleContent/SingleContent.jsx
@@ -1,9 +1,14 @@
 import './singlecontent.css';
 import { img_300, unavailable } from "../../config/config";
+import { Badge } from "@material-ui/core";
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average}) => {
   return (
      <div className="media">
+       <Badge
+        badgeContent={vote_average}
+        color={vote_average > 6 ? "primary" : "secondary"}
+      />
        <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
        <b className="title">{title}</b>
        <span className="subtitle">
