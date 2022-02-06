e71e6ea37ad8c1ad11a38347c6b38124d17fbbaa
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 14:17:27 -0500

    create fetch function trending

--- src/pages/Trending/Trending.jsx
+++ src/pages/Trending/Trending.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+
 const Trending = () => {
+
+  const [content, setContent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+
+      setContent(data.results);
+  }
+
   return (
    <div>
        <span className="pageTitle">Trending</span>
