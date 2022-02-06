4c598fd9647ed4be123b2e4aeebf2abf48b521fa
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 13:51:08 -0500

    replace useHistory with useNavigate

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -5,7 +5,7 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import SearchIcon from '@mui/icons-material/Search';
 import TvIcon from '@mui/icons-material/Tv';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
@@ -21,14 +21,14 @@ const BottomNav = () => {
 
 const classes = useStyles();
 const [value, setValue] = useState(0);
-const history = useHistory();
+const navigate = useNavigate();
 
 useEffect(() => {
-    if (value === 0) history.pushState("/");
-    else if ( value === 1) history.push("/movies");
-    else if ( value === 2) history.push("/series");
-    else if ( value === 3) history.push("/search");
-}, [value, history]);
+    if (value === 0) navigate("/");
+    else if ( value === 1) navigate("/movies");
+    else if ( value === 2) navigate("/series");
+    else if ( value === 3) navigate("/search");
+}, [value, navigate]);
 
   return (
     <BottomNavigation
