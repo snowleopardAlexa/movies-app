8be6ec3747e063926af0e687231acbc04e1353eb
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 13:17:05 -0500

    import components in app js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Header from './components/Header/Header';
 import BottomNav from './components/BottomNav/BottomNav';
+import Trending from './pages/Trending/Trending';
+import Movies from './pages/Trending/Trending';
+import Series from './pages/Trending/Trending';
+import Search from './pages/Trending/Trending';
 
 function App() {
   return (
