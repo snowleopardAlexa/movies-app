1c140cb6661595bae3171047b7ce433f055f693d
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 13:12:21 -0500

    add react router routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import Header from './components/Header/Header';
 import BottomNav from './components/BottomNav/BottomNav';
 
 function App() {
   return (
     <>
+    <Routes>
     <Header />
     <div className="app">
-      <BottomNav />
+      <Container>
+        <Route path="/" element={<Trending />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/series" element={<Series />} />
+        <Route path="/search" element={<Search />} />
+      </Container>
     </div>
+    <BottomNav />
+    </Routes>
     </>
   );
 }
