f7236e472b9d8d0b2d6cfff33ae609f93b0c0eb1
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 20:02:31 -0500

    add use effect

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,20 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './movies.css';
 
 const Movies = () => {
+
+  const [page, setPage] = useState(1);
 
   const fetchMovies = async () => {
       const { data } = await axios.get (
           `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
       );
   };
+
+  useEffect(() => {
+      fetchMovies();
+  }, []);
 
     return (
      <div>
