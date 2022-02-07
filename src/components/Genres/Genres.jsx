2b43728985f97bc21d6905e39fcb10fbd22e8431
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 20:46:57 -0500

    add fetch genres and use effect to fetch genres

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,6 +1,22 @@
 import './genres.css';
 
 const Genres = ({ selectedGenres, setSelectedGenres, setGenres, type, setPage}) => {
+
+  const fetchGenres =  async () => {
+      const { data } = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setGenres(data.genres);
+  }
+
+  useEffect(() => {
+      fetchGenres();
+
+      return () => {
+          setGenres({});
+       }
+  }, []);
+
   return (
     <div>Genres</div>
   );
