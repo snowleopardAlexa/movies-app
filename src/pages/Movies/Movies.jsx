af8bd0eb6d382699602316f525e4d25b64bc36d3
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 20:05:25 -0500

    add state for pages and content

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -5,15 +5,21 @@ import './movies.css';
 const Movies = () => {
 
   const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
 
   const fetchMovies = async () => {
       const { data } = await axios.get (
           `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
       );
+
+      setContent(data.result)
+      setNumOfPages(data.total_pages);
   };
 
   useEffect(() => {
       fetchMovies();
+       // eslint-disable-next-line
   }, []);
 
     return (
