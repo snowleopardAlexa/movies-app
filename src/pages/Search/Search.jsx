712a64c663fb560a02d4a84a9801ce91c67ed6d2
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 21:47:39 -0500

    add dark theme and all imports

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,41 @@
+import {
+    Button,
+    createTheme,
+    Tab,
+    Tabs,
+    TextField,
+    ThemeProvider,
+  } from "@material-ui/core";
+  import "./Search.css";
+  import SearchIcon from "@material-ui/icons/Search";
+  import { useEffect, useState } from "react";
+  import axios from "axios";
+  import CustomPagination from "../../components/Pagination/CustomPagination";
+  import SingleContent from "../../components/SingleContent/SingleContent";
+
 const Search = () => {
+
+    const [type, setType] = useState(0);
+
+    const darkTheme = createTheme({
+        palette: {
+          type: "dark",
+          primary: {
+            main: "#fff",
+          },
+        },
+    });
+
     return (
      <div>
          <span className="pageTitle">Search</span>
+         <TextField 
+           style={{ flex: 1}}
+           className="searchBox"
+           label="Search"
+           variant="filled"
+           // onChange={(e) => setSearchText(e.target.value)}
+         />
      </div>
     );
   };
