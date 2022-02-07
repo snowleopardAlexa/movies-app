46dc87de22004ca1853ce8ea6f20570d457063ec
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 19:59:30 -0500

    write fetch movies function

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,7 +1,17 @@
+import axios from 'axios';
+import './movies.css';
+
 const Movies = () => {
+
+  const fetchMovies = async () => {
+      const { data } = await axios.get (
+          `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      );
+  };
+
     return (
      <div>
-         <span className="pageTitle">M</span>
+         <span className="page-title">Movies</span>
      </div>
     );
   };
