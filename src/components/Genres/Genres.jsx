5e2731bcf7604a36e32a660326cc43207889d617
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-06 20:57:58 -0500

    pass genres as props

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,24 +1,46 @@
-import './genres.css';
+import { useState, useEffect } from "react";
+import "./genres.css";
+import axios from "axios";
+import { Chip } from "@material-ui/core";
 
-const Genres = ({ selectedGenres, setSelectedGenres, setGenres, type, setPage}) => {
-
-  const fetchGenres =  async () => {
-      const { data } = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+const Genres = ({
+  genres,
+  selectedGenres,
+  setSelectedGenres,
+  setGenres,
+  type,
+  setPage,
+}) => {
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
     );
 
     setGenres(data.genres);
-  }
+  };
 
   useEffect(() => {
-      fetchGenres();
+    fetchGenres();
 
-      return () => {
-          setGenres({});
-       }
+    return () => {
+      setGenres({}); // unmounting
+    };
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <div>Genres</div>
+    <div className="genre">
+      {genres &&
+        genres.map((genre) => (
+          <Chip
+            label={genre.name}
+            style={{ margin: 2 }}
+            clickable
+            size="small"
+            key={genre.id}
+          />
+        ))}
+    </div>
   );
 };
 
